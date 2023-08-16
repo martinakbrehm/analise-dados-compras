80ca2df3222d486756e34513948339bb6b9f0aa1
Martina <48300488+martinakbrehm@users.noreply.github.com>
2023-08-16 14:27:40 -0300

    Add files via upload

--- segmentacao_genero.py
+++ segmentacao_genero.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 df = pd.read_json('dados_compras.json') #ler onde arquivo está
 df.head() #ler linhas do arquivo
 
+#analise de dados
+
 informacoes_compradores = df.loc[:, ["Sexo", "Login", "Idade"]]
 informacoes_compradores.head()
 
@@ -12,17 +14,9 @@ informacoes_compradores = informacoes_compradores.drop_duplicates()
 contagem_compradores = informacoes_compradores.count()[0]
 
 
-
 total_gasto_por_genero = df.groupby(["Sexo"]).sum()["Valor"].rename("Valor Total Gasto em Compras")
 
 print(total_gasto_por_genero)
-legenda_total_por_genero = ["Feminino", "Masculino", "Outro / Não Divulgado"]
-fig = plt.figure(figsize =(7, 7))
-plt.subplot(1, 2, 1)
-plt.pie(total_gasto_por_genero, labels = total_gasto_por_genero)
-plt.legend(legenda_total_por_genero)
-plt.title("Valores gastos por genero")
-
 
 contagem_genero = informacoes_compradores["Sexo"].value_counts()
 porcentagem_genero = (contagem_genero / contagem_compradores) * 100
@@ -30,11 +24,18 @@ porcentagem_genero = (contagem_genero / contagem_compradores) * 100
 porcentagem_genero = round(porcentagem_genero, 2)
 
 informacoes_compradores = pd.DataFrame({"Número de compradores" : contagem_genero, "%" : porcentagem_genero})
-
-plt.subplot(1, 2, 2)
 print(informacoes_compradores)
 
+#visualização de dados
 
+fig = plt.figure(figsize =(10, 10))
+legenda_total_por_genero = ["Feminino", "Masculino", "Outro / Não Divulgado"]
+plt.subplot(1, 2, 1)
+plt.pie(total_gasto_por_genero, labels = total_gasto_por_genero)
+plt.legend(legenda_total_por_genero)
+plt.title("Valores gastos por genero")
+
+plt.subplot(1, 2, 2)
 legenda_porcentagem_genero = ["Masculino", "Feminino", "Outro / Não Divulgado"]
 plt.pie(porcentagem_genero, labels = porcentagem_genero)
 plt.legend(legenda_porcentagem_genero)
